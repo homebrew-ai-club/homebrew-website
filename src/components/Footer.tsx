0b875bbf545a8051b9067b22523eb87a35ee942c
Kaival Shah <kaival.s.shah@gmail.com>
2025-05-10 22:57:18 -0500

    update homebrew footer socials and creds

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
             <div className="px-5 my-6 flex flex-row items-end justify-between">
                 <div className="flex flex-col">
                     <h2>Want to Join Homebrew?</h2>
-                    <p className="mb-4 max-w-[60vw]">We review applications on a rolling basis throughout the quarter. Applications typically take less than a week to review.</p>
+                    <p className="mb-4 max-w-[60vw]">Tell us about yourself! This is an opportunity for us to get to know your interests, goals, and what you're passionate about building.</p>
                     <p>
                         <Link
                             href="https://tally.so/r/mZggez"
@@ -18,15 +18,25 @@ export default function Footer() {
                     </p>
                 </div>
                 <div className="flex flex-col items-end gap-5">
-                    <Link href="https://instagram.com">
-                        <Image
-                            src="/icons/instagram.png"
-                            alt="instagram icon"
-                            width={30}
-                            height={30}
-                        />
-                    </Link>
-                    <p>Website by <Link href="https://andrew-yuan.com">Andrew Yuan</Link>.</p>
+                    <div className="flex flex-row items-end gap-5">
+                        <Link href="https://www.linkedin.com/company/homebrew-nu">
+                            <Image
+                                src="/icons/linkedin.png"
+                                alt="linkedin icon"
+                                width={30}
+                                height={30}
+                            />
+                        </Link>
+                        <Link href="https://www.instagram.com/homebrew.nu">
+                            <Image
+                                src="/icons/instagram.png"
+                                alt="instagram icon"
+                                width={30}
+                                height={30}
+                            />
+                        </Link>
+                    </div>
+                    <p>Website made with 🤎 by the Homebrew team.</p>
                 </div>
             </div>
         </div>
